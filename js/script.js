dd7b74689d5c3f782ec08d610211774bc902a0d2
Monirul <monirul.haque@g.bracu.ac.bd>
2020-09-05 22:11:25 +0600

    Added javascript and added premium seta class in html

--- js/script.js
+++ js/script.js
@@ -1,2 +1,32 @@
 const container = document.querySelector(".container");
-const seats = document.querySelectorAll(".seat");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+
+let ticketPrice = +movieSelect.value;
+
+//Movie select event
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = +movieSelect.value;
+  updateSelectedCount();
+});
+
+//Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+container.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+    updateSelectedCount();
+  }
+});
